3778942f854a42a200fb140fb52da71a3a598983
aleks <aleks.mihaylov69@gmail.com>
2021-09-27 14:25:55 +0300

    add fire emoji

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,8 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  const hotPrices = document.querySelectorAll(".hot");
+  hotPrices.forEach((p) => {
+    p.textContent = p.textContent + "🔥";
+  });
 });
